c971527ffe4d1bc3adeddfbd5b9de938e702bbf0
smunpov1010 <smunpov1010@PC111918.dptoinformatica.iesfuengirola1.net>
2025-04-10 18:45:01 +0200

    commit

--- BD_Instituto/src/bd_instituto/BD_Instituto.java
+++ BD_Instituto/src/bd_instituto/BD_Instituto.java
@@ -5,6 +5,10 @@ public class BD_Instituto {
 
 
     public static void main(String[] args){
+      GestionAlumnos ga = new GestionAlumnos();
       
+      ga.agregarAlumno();
+      
+      ga.verAlumnos();
     }
 }
