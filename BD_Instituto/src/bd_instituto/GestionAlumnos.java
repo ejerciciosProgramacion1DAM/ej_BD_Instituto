baba543757b793bc377ebf7d0dcf38aaf49b4a74
samue <samue@PC_Samuel>
2025-04-12 17:03:55 +0200

    Clase Gestion: metodo de elimiar acabado y hecho entero el de modificar

--- BD_Instituto/src/bd_instituto/GestionAlumnos.java
+++ BD_Instituto/src/bd_instituto/GestionAlumnos.java
@@ -4,15 +4,26 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GestionAlumnos {
 
-    //atributos
-    Scanner in = new Scanner(System.in);
+    //meno de las opciones para modificar
+    public static void menu() {
+        System.out.println("\n==========================");
+        System.out.println("=====Modificar Alumno====");
+        System.out.println("==========================");
+        System.out.println("___Opciones___");
+        System.out.println("1.Nombre");
+        System.out.println("2.Nota media");
+        System.out.println("3.Curso");
+        System.out.println("4.Salir");
+    }
 
-    //metodos de manipulacion, edicion y revision de la base de datos
-    public void verAlumnos() {
+    //Metodos de manipulacion, edicion y revision de la base de datos
+    //mostrar un listado de todos los alumnos
+    public static void verAlumnos() {
         Connection conexion = ConexionBaseDatos.getConnection();
         if (conexion != null) {
             try {
@@ -23,7 +34,7 @@ public class GestionAlumnos {
                 while (resultado.next()) {
                     System.out.println("Id: " + resultado.getInt("id"));
                     System.out.println("Nombre: " + resultado.getNString("Nombre"));
-                    System.out.println("Nota media: " + resultado.getInt("Nota_media"));
+                    System.out.println("Nota media: " + resultado.getFloat("Nota_media"));
                     System.out.println("Curso: " + resultado.getNString("Curso"));
                 }
                 conexion.close();
@@ -33,7 +44,8 @@ public class GestionAlumnos {
         }
     }
 
-    public void agregarAlumno() {
+    //agregar a un alumno nuevo a la base de datos
+    public static void agregarAlumno(Scanner in) {
         Connection conexion = ConexionBaseDatos.getConnection();
 
         if (conexion != null) {
@@ -54,16 +66,70 @@ public class GestionAlumnos {
         }
     }
 
-    public void eliminarAluumno() {
+    //eliminar a un alumno nuevo a la base de datos
+    public static void eliminarAluumno(Scanner in) {
+        Connection conexion = ConexionBaseDatos.getConnection();
         try {
-            System.out.print("Cual es el id del usuario qeu desea eliminar: ");
+            System.out.print("Cual es el id del usuario que desea eliminar: ");
             int iId = in.nextInt();
 
-        } catch (Exception e) {
+            var stmt = conexion.prepareStatement("DELETE FROM alumno WHERE alumno.id = ?");
+            stmt.setInt(1, iId);
+            stmt.execute();
+            System.out.println("Alumno eliminado correctamente");
+
+        } catch (InputMismatchException e) {
+            System.out.println("ha ocurrido un error al ingresar el numero: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Ha ocurrido un error en la base de datos: " + e.getMessage());
         }
     }
 
-    public void modificarAluumno() {
+    //actualizar un valir de un alumno de la base de datos
+    public static void modificarAlumno(Scanner in) {
+        Connection conexion = ConexionBaseDatos.getConnection();
+        try {
+            System.out.print("Cual es el id del usuario que desea modificar: ");
+            int iId = in.nextInt();
 
+            menu();
+            System.out.print("Que valo va a modificar: ");
+            int opci = in.nextInt();
+            in.nextLine();
+
+            switch (opci) {
+                case 1 -> {
+                    String nombre = Alumno.setsNombre(in);
+                    var stmt = conexion.prepareStatement("UPDATE alumno SET Nombre=? WHERE id=?");
+                    stmt.setString(1, nombre);
+                    stmt.setInt(2, iId);
+                    stmt.execute();
+                }
+                case 2 ->{
+                    float nota = Alumno.setfNota(in);
+                    var stmt = conexion.prepareStatement("UPDATE alumno SET Nota_media=? WHERE id=?");
+                    stmt.setFloat(1, nota);
+                    stmt.setInt(2, iId);
+                    stmt.execute();
+                }
+                
+                case 3 ->{
+                    String curso = Alumno.setsCurso(in);
+                    var stmt = conexion.prepareStatement("UPDATE alumno SET Curso=? WHERE id=?");
+                    stmt.setString(1, curso);
+                    stmt.setInt(2, iId);
+                    stmt.execute();
+                }
+                case 4 ->{System.out.println("Saliendo...");}
+                default -> System.out.println("Opcion inexistente :/");
+            }
+            
+        } catch (InputMismatchException e) {
+            System.out.println("ha ocurrido un error al ingresar el numero: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Ha ocurrido un error en la base de datos: " + e.getMessage());
+        }
     }
+    
+    
 }
