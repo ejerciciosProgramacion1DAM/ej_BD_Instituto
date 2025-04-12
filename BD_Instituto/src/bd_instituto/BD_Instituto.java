a63006d7d56ab712a1ec8d753d0b642fb77a05a3
samue <samue@PC_Samuel>
2025-04-12 17:02:43 +0200

    Commit: Main(llamo a los metodos para ver si funcionan)

--- BD_Instituto/src/bd_instituto/BD_Instituto.java
+++ BD_Instituto/src/bd_instituto/BD_Instituto.java
@@ -1,14 +1,15 @@
 package bd_instituto;
 
+import java.util.Scanner;
 
 public class BD_Instituto {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-    public static void main(String[] args){
-      GestionAlumnos ga = new GestionAlumnos();
-      
-      ga.agregarAlumno();
-      
-      ga.verAlumnos();
+        GestionAlumnos.agregarAlumno(in);
+        GestionAlumnos.eliminarAluumno(in);
+        GestionAlumnos.modificarAlumno(in);
+        GestionAlumnos.verAlumnos();
     }
 }
